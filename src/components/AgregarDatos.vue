7addc79de865a8e0dc422b40eeba1dfceacab452
diegocmd20 Home <diegocmd20@gmail.com>
2024-12-02 20:08:00 -0300

    Props added

--- src/components/AgregarDatos.vue
+++ src/components/AgregarDatos.vue
@@ -1,17 +1,30 @@
 <template>
+    <div class="container my-1">
+        <h6>Paciente</h6>
+        <p>{{ paciente }}</p>
+        <h6>Fecha</h6>
+        <p>{{ fecha }}</p>
+        <h6>Hora</h6>
+        <p>{{ hora }}</p>
+        <h6>Gravedad</h6>
+        <p>{{ gravedad }}</p>
+        <h6>Motivo</h6>
+        <p>{{ motivo }}</p>
+        <button type="submit">Eliminar</button>
+    </div>
 </template>
 
 <script>
 export default {
     name: 'AgregarDatos',
-    data() {
-        return {
-        };
-    }, methods:{
-
+    props: {
+        paciente: String,
+        fecha: Date,
+        hora: Date,
+        gravedad: String,
+        motivo: String,
     }
 };
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
--- src/components/Cita.vue
+++ src/components/Cita.vue
@@ -1,47 +1,58 @@
 <template>
     <div class="border border-secondary rounded border-1 py-1">
         <div class="d-flex align-items-center justify-content-center">
-            
-                <div class="d-flex flex-column my-1 mx-2">
-                    <label for="paciente" class="form-label fst-semibold">Paciente</label>
-                    <input name="paciente" type="text" class="form-control">
-                </div>
-                <div class="d-flex flex-column my-1 mx-2">
-                    <label for="fecha" class="form-label fst-semibold">Fecha</label>
-                    <input type="date" name="fecha">
-                </div>
-                <div class="d-flex flex-column my-1 mx-2">
-                    <label for="hora" class="form-label fst-semibold">Hora</label>
-                    <input type="time" name="hora">
-                </div>
-                <div class="d-flex flex-column my-1 mx-2">
-                    <label for="gravedad" class="form-label">Gravedad</label>
-                    <select name="gravedad">
-                        <option value="" selected>Elige la gravedad</option>
-                        <option value="bg-success">Baja</option>
-                        <option value="bg-warning">Media</option>
-                        <option value="bg-danger">Grave</option>
-                    </select>
-                </div>
-                <div class="d-flex flex-column my-1 mx-2">
-                    <label for="motivo">Motivo</label>
-                    <input name="motivo" type="text" class="form-control">
-                </div>
+
+            <div class="d-flex flex-column my-1 mx-2">
+                <label for="paciente" class="form-label fst-semibold">Paciente</label>
+                <input v-model="paciente" name="paciente" type="text" class="form-control" placeholder="Nombre del Paciente">
+            </div>
+            <div class="d-flex flex-column my-1 mx-2">
+                <label for="fecha" class="form-label fst-semibold">Fecha</label>
+                <input v-model="fecha" type="date" name="fecha">
+            </div>
+            <div class="d-flex flex-column my-1 mx-2">
+                <label for="hora" class="form-label fst-semibold">Hora</label>
+                <input v-model="hora" type="time" name="hora">
+            </div>
+            <div class="d-flex flex-column my-1 mx-2">
+                <label for="gravedad" class="form-label">Gravedad</label>
+                <select v-model="gravedad" name="gravedad">
+                    <option value="" selected>Elige la gravedad</option>
+                    <option value="bg-success">Baja</option>
+                    <option value="bg-warning">Media</option>
+                    <option value="bg-danger">Grave</option>
+                </select>
+            </div>
+            <div class="d-flex flex-column my-1 mx-2">
+                <label for="motivo">Motivo</label>
+                <input v-model="motivo" name="motivo" type="text" class="form-control">
+            </div>
 
         </div>
         <button type="submit">Agregar</button>
+        <div>
+            <AgregarDatos :paciente="paciente" :fecha="fecha" :hora="hora" :gravedad="gravedad" :motivo="motivo"/>
+        </div>
     </div>
 </template>
 
 <script>
+import AgregarDatos from './AgregarDatos.vue';
 export default {
     name: 'Cita',
     data() {
         return {
-
+            paciente: '',
+            fecha: '',
+            hora: '',
+            gravedad: '',
+            motivo:'',
         };
     },
-    computed:{
+    components: {
+        AgregarDatos,
+    },
+    methods: {
 
     }
 };
